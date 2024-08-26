2a3191ede09632a719b491297e87c94674824f37
Miron-Anosov <mairon26rus@gmail.com>
2024-08-26 17:11:21 +0200

    docs(init): create main module docs
    Functional ability API

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""Main FastAPI module.
+
+It here will be documentation about major information of API.
+
+Functional ability:
+    - User can add new post.
+    - User can delete post.
+    - User can follow another users.
+    - User can unfollow another users.
+    - User can mark post.
+    - User can unmark post.
+    - User can receive news from posts in descending
+                                 order of popularity from users they follow.
+    - A post can include a picture.
+
+"""
