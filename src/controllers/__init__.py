3e3be32ba8d2c4b957564f3bdf6ee066200668d9
Miron-Anosov <mairon26rus@gmail.com>
2024-08-26 23:37:13 +0200

    feat(test_app): create packege
    Add a doc

---
+++ src/controllers/__init__.py
@@ -0,0 +1,12 @@
+"""
+This package provides API endpoints for the MicroBlog application.
+
+Endpoints:
+    - POST /api/tweets
+    - POST /api/medias
+    - DELETE /api/tweets/<id>
+    - POST /api/tweets/<id>/likes
+    - DELETE /api/tweets/<id>/likes
+    - POST /api/users/<id>/follow
+    - DELETE /api/users/<id>/follow
+"""
