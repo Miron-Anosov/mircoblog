4fde64fdf8aece93dab396b6ed9a3eca9b6bb080
Miron <miron.nicolaevich@gmail.com>
2024-08-27 22:20:31 +0200

    dosc(test_app): create doc for package of controllers

---
+++ src/back_core/controllers/__init__.py
@@ -0,0 +1,17 @@
+"""
+This package provides API endpoints for the MicroBlog application.
+
+    Controllers:
+        tweets:
+          - POST /api/tweets
+          - POST /api/tweets/<id>/likes
+          - DELETE /api/tweets/<id>/likes
+          - DELETE /api/tweets/<id>
+
+        media:
+          - POST /api/medias
+
+        users:
+         - POST /api/users/<id>/follow
+         - DELETE /api/users/<id>/follow
+"""
