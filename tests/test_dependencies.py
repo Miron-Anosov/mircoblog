4312a9ac241f34d922a2abd5d5a8cd4f475865c8
Miron <miron.nicolaevich@gmail.com>
2024-09-02 17:46:42 +0200

    refactor(test_app); change test settings

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -11,18 +11,6 @@ from src.settings.settings import CommonSettings, settings
 
 
 @pytest.mark.test_config
-def test_dependencies_installed():
-    """Test env config."""
-    assert settings.test.ECHO_TEST is False
-
-
-@pytest.mark.test_config
-def test_dependencies_installed_1():
-    """Test env config."""
-    assert settings.test.get_url_database
-
-
-@pytest.mark.test_config
 def test_dependencies_installed_2():
     """Test env config."""
     with pytest.raises(TypeError):
