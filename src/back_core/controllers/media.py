1977d43770d1150e0c8fe4a0807952044a4e6ff8
Miron <miron.nicolaevich@gmail.com>
2024-09-03 00:32:37 +0200

    docs(test_app): change docs

--- src/back_core/controllers/media.py
+++ src/back_core/controllers/media.py
@@ -2,10 +2,10 @@
 
 from fastapi import APIRouter
 
-media = APIRouter(prefix="/api", tags=["Media"])
+media = APIRouter(tags=["Media"])
 
 
 @media.post("/media")
-def get_status() -> dict[str, str]:
+async def get_status() -> dict[str, str]:
     """Test route."""
     return {"message": "Hello World"}
