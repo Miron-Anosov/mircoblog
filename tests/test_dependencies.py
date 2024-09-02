6d79536863a5eb736e6edc5bc0567fb8a727c813
Miron <miron.nicolaevich@gmail.com>
2024-09-02 16:57:42 +0200

    refactor(test_app); add test settings

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -2,8 +2,28 @@
 
 import pytest
 
+from src.settings.settings import CommonSettings, settings
 
-@pytest.mark.test
+# import httpx
+# import pytest_asyncio
+# from asgi_lifespan import LifespanManager
+# from httpx import codes
+
+
+@pytest.mark.test_config
 def test_dependencies_installed():
-    """Test."""
-    assert True
+    """Test env config."""
+    assert settings.test.ECHO_TEST is False
+
+
+@pytest.mark.test_config
+def test_dependencies_installed_1():
+    """Test env config."""
+    assert settings.test.get_url_database
+
+
+@pytest.mark.test_config
+def test_dependencies_installed_2():
+    """Test env config."""
+    with pytest.raises(TypeError):
+        _ = CommonSettings().get_url_database
