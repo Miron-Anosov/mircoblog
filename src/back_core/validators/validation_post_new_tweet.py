9a94b1f6632744f67ddef1cd81ddc4f2f7754111
Miron <miron.nicolaevich@gmail.com>
2024-09-05 13:16:02 +0200

    feat(routes_api): create validator model ValidPostModelNewTweetOutput

--- src/back_core/validators/validation_post_new_tweet.py
+++ src/back_core/validators/validation_post_new_tweet.py
@@ -3,17 +3,35 @@
 You can see route to /src/back_core/controllers/tweets.py
 """
 
+import uuid
+
 import pydantic
 
 
 class ValidPostModelNewTweetInput(pydantic.BaseModel):
-    """Model validate input date from route /tweets.
+    """**Model validate input date from route /tweets**.
 
-    tweet_data: str: Data post tweet.
-    tweet_media_ids: Pictures id with tweet.
+    - `tweet_data`: str: Data post tweet.
+    - `tweet_media_ids`: Pictures id with tweet.
     """
 
     tweet_data: str = pydantic.Field(description="Message to new post")
     tweet_media_ids: list[int] | None = pydantic.Field(
-        strict=False, description="Array tweet ids"
+        strict=False, description="Array tweet IDs"
+    )
+
+
+class ValidPostModelNewTweetOutput(pydantic.BaseModel):
+    """**Movel validate response /tweets**.
+
+    - `result`: bool : Successful or unsuccessful.
+    - `tweet_id`: UUID : Unique identifier for the tweet.
+    """
+
+    result: bool = pydantic.Field(
+        description="Successful or unsuccessful",
+    )
+    tweet_id: uuid.UUID = pydantic.Field(
+        default_factory=uuid.uuid4,
+        description="Unique identifier for " "the tweet.",
     )
