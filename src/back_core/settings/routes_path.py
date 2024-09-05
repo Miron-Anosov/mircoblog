af5edcd4953913f311453512797941bb005e5583
Miron-Anosov <mairon26rus@gmail.com>
2024-09-05 17:58:56 +0200

    feat(routes_api): add paths in PathRoutes

--- src/back_core/settings/routes_path.py
+++ src/back_core/settings/routes_path.py
@@ -11,5 +11,7 @@ class PathRoutes(enum.Enum):
 
     PREFIX = "/api"
     TWEETS = "/tweets"
-    TWEETS_DEL_BY_ID = "/tweets{tweet_id}"
+    TWEETS_ID_LIKE = "/tweets/{tweet_id}/like"
+    TWEETS_DEL_BY_ID = "/tweets/{tweet_id}"
+    TWEETS_DEL_LIKE_BY_ID = "/tweets/{tweet_id}/like"
     USERS = "/users"
