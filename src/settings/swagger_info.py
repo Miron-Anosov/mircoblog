8bea815d1dec8884d73ea035d3ff1aee80b2d400
Miron <miron.nicolaevich@gmail.com>
2024-09-05 15:38:55 +0200

    docs(routes_api): get info fron environment now

--- src/settings/swagger_info.py
+++ src/settings/swagger_info.py
@@ -2,6 +2,8 @@
 
 from pathlib import Path
 
+from .settings import settings
+
 SUMMARY = (
     "This API working with MicroBlog data."
     "Functional ability:\
@@ -21,16 +23,28 @@ DESCRIPTION = Path(_file_description_path).read_text()
 TAGS_METADATA = [
     {
         "name": "Methods API",
-        "description": "Попробуй это.",
+        "description": "Try use this.",
     },
 ]
 
 CONTACT = {
-    "name": "Miron",
-    "url": "https://gitlab.com/miron.nicolaevich/",
-    "email": "miron-nicolaevich@gmail.com",
+    "name": settings.prod.CONTACT_NAME,
+    "url": settings.prod.CONTACT_URL,
+    "email": settings.prod.CONTACT_EMAIL,
 }
 
 SERVERS = [{"url": "http://localhost:8000"}]
 
-__all__ = ["SUMMARY", "DESCRIPTION", "TAGS_METADATA", "CONTACT", "SERVERS"]
+VERSION_API = settings.prod.VERSION_API
+
+TITLE = "MicroBlog API"
+
+__all__ = [
+    "SUMMARY",
+    "DESCRIPTION",
+    "TAGS_METADATA",
+    "CONTACT",
+    "SERVERS",
+    "VERSION_API",
+    "TITLE",
+]
