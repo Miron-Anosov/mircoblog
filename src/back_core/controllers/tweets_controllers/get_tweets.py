d8fdcf8d654ecd80d50f808081653e79bfc68f1a
Miron-Anosov <mairon26rus@gmail.com>
2024-09-07 23:32:52 +0200

    refactor(routes_api): decompile route of tweets

---
+++ src/back_core/controllers/tweets_controllers/get_tweets.py
@@ -0,0 +1,11 @@
+"""GET /tweets/."""
+
+
+def get_tweets():
+    """
+    Get tweets.
+
+     **Headers**:
+    - `api_key (str)*`: API key for authentication.
+    """
+    return  # TODO: заглушка, прикрутить CRUD
