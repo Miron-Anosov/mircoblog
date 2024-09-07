5f10b754c6d20ae06c7ecc490960c6b728b67be9
Miron-Anosov <mairon26rus@gmail.com>
2024-09-07 19:11:55 +0200

    feat(rotes_api): creat ValidUserModelmodel

---
+++ src/back_core/validators/valid_user.py
@@ -0,0 +1,18 @@
+"""Validator models for /tweets.
+
+You can see route to /src/back_core/controllers/users.py
+"""
+
+import uuid
+
+import pydantic
+
+
+class ValidUserModel(pydantic.BaseModel):
+    """Model for tweet author details."""
+
+    id: uuid.UUID = pydantic.Field(
+        default_factory=uuid.uuid4,
+        description="Author's unique ID",
+    )
+    name: str = pydantic.Field(..., description="Author's name")
