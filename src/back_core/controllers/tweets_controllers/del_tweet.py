04e8478469dc0b1b57ba099abb196b64bd6f7e5d
Miron <miron.nicolaevich@gmail.com>
2024-09-07 23:32:52 +0200

    refactor(routes_api): decompile route of tweets

---
+++ src/back_core/controllers/tweets_controllers/del_tweet.py
@@ -0,0 +1,17 @@
+"""DELETE /tweets/{id}.
+
+Delete tweet.
+"""
+
+
+def del_tweet_by_id(tweet_id: str):
+    """
+    Delete a tweet by ID.
+
+     **Headers**:
+    - `api_key (str)*`: API key for authentication.
+
+    **Path Parameters**:
+    - `tweet_id (int)`: The ID of the tweet to delete.
+    """
+    pass  # TODO: заглушка, прикрутить CRUD
