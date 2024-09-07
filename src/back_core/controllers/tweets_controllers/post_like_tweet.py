b93d3e111444fa1874e2c086e304c8a32be1d359
Miron-Anosov <mairon26rus@gmail.com>
2024-09-07 23:32:52 +0200

    refactor(routes_api): decompile route of tweets

---
+++ src/back_core/controllers/tweets_controllers/post_like_tweet.py
@@ -0,0 +1,17 @@
+"""POST /tweets/{id}/like.
+
+Create like of tweet.
+"""
+
+
+def post_like_by_id(tweet_id: str):
+    """
+    Like a tweet by ID.
+
+     **Headers**:
+    - `api_key (str)*`: API key for authentication.
+
+    **Path Parameters**:
+    - `tweet_id (int)`: The ID of the tweet to like.
+    """
+    return  # TODO: заглушка, прикрутить CRUD
