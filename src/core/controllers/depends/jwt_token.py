6ddefa9186be8418eb7a7311af332e543b8e4c8a
Miron <miron.nicolaevich@gmail.com>
2024-09-27 22:11:13 +0200

    feat(database): create gen jwt token

---
+++ src/core/controllers/depends/jwt_token.py
@@ -0,0 +1,47 @@
+"""TODO DOCS."""
+
+import datetime
+
+import jwt
+
+from src.core.settings.settings import settings
+
+
+def encode_jwt(
+    payload: dict,
+    private_key: str = settings.jwt_tokens.private_token.read_text(),
+    algorithm: str = settings.jwt_tokens.algorithm,
+    expire_minutes: int = settings.jwt_tokens.access_token_expire_minutes,
+    expire_delta: datetime.timedelta | None = None,
+) -> str:
+    """Return encoded token."""
+    now = datetime.datetime.now(datetime.UTC)
+
+    if expire_delta:
+        expire = now + expire_delta
+    else:
+        expire = now + datetime.timedelta(minutes=expire_minutes)
+
+    to_encode = payload.copy()
+    to_encode["exp"] = expire
+    to_encode["iat"] = now
+    encode = jwt.encode(
+        payload=payload,
+        key=private_key,
+        algorithm=algorithm,
+    )
+    return encode
+
+
+def decode_jwt(
+    jwt_token: str | bytes,
+    public_key: str = settings.jwt_tokens.public_token.read_text(),
+    algorithm: str = settings.jwt_tokens.algorithm,
+) -> str:
+    """Return decoded token."""
+    decoded = jwt.decode(
+        jwt=jwt_token,
+        key=public_key,
+        algorithms=[algorithm],
+    )
+    return decoded
