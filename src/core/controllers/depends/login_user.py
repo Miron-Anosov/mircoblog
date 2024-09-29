c24c3038550b2771f35ced1d02bd1faf513efde2
Miron-Anosov <mairon26rus@gmail.com>
2024-09-29 22:00:45 +0200

    feat(database): create login_form

--- src/core/controllers/depends/login_user.py
+++ src/core/controllers/depends/login_user.py
@@ -2,7 +2,9 @@
 
 from typing import TYPE_CHECKING, Annotated
 
-from fastapi import Depends, HTTPException, status
+import pydantic
+from fastapi import Depends, Form, HTTPException, status
+from fastapi.security import HTTPBearer
 
 from src.core.controllers.depends.connect_db import get_crud, get_session
 from src.core.controllers.depends.hash_password import validate_pwd
@@ -53,9 +55,67 @@ async def login_user_json(
             "username": user_profile.name,
         }
 
-        new_token = encode_jwt(payload=payload)
+        return UserToken(access_token=encode_jwt(payload=payload))
 
-        return UserToken(access_token=new_token)
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid email or password",
+    )
+
+
+async def login_user_form(
+    email: Annotated[
+        pydantic.EmailStr,
+        Form(
+            description="User's email.",
+        ),
+    ],
+    password: Annotated[
+        str,
+        Form(
+            min_length=1,
+            max_length=64,
+            description="User's secret.",
+        ),
+    ],
+    crud: Annotated["Crud", Depends(get_crud)],
+    session: Annotated["AsyncSession", Depends(get_session)],
+) -> "UserToken":
+    """Check user in DB.
+
+    Args:
+        email: str: user_email
+        password: srt: secret
+        crud: Crud()
+        session: AsyncSession
+    Return:
+        UserToken(access_token: str, token_type: str)
+    Raises:
+        HTTPException()
+    Notes:
+        Return new JWT token.
+    """
+    user_hash_pwd, user_id = await crud.auth_users.login_user(
+        email=email, session=session
+    )
+
+    if user_hash_pwd and validate_pwd(
+        password=password,
+        hash_password=user_hash_pwd.encode("utf-8"),
+    ):
+        # TODO ADD LOGER DEBUG
+
+        user_profile = await crud.users.get_user(
+            id_user=user_id,
+            session=session,
+        )
+
+        payload = {
+            "sub": user_profile.name,
+            "username": user_profile.name,
+        }
+
+        return UserToken(access_token=encode_jwt(payload=payload))
 
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
