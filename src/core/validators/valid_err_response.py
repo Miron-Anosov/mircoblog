e0114ac534c2cc233f58dd23849b347df31768d3
Miron <miron.nicolaevich@gmail.com>
2024-09-30 11:11:14 +0200

    docs(database): add docs at ValidErrResponse

--- src/core/validators/valid_err_response.py
+++ src/core/validators/valid_err_response.py
@@ -7,6 +7,10 @@ class ValidErrResponse(pydantic.BaseModel):
     """Response error.
 
     If Api will have any errors, it will send to client.
+    Args:
+        result (bool): default False
+        error_type (str): description error
+        error_message (str): message error
     """
 
     result: bool = False
