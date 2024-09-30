83d34d84e28b004140096ebd655876b9ec383ba6
Miron <miron.nicolaevich@gmail.com>
2024-09-30 18:07:59 +0200

    refactor(database): change bearer class

--- tests/integration/test_headers.py
+++ tests/integration/test_headers.py
@@ -21,7 +21,7 @@ async def test_get_user_profile_without_api_key(client: AsyncClient):
         response = await client.get(
             f"{UsersRoutes.PREFIX}{UsersRoutes.GET_ME}"
         )
-        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
 # @pytest.mark.asyncio
