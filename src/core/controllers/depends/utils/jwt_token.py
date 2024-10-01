61b9c6a2881836029aa387ab11739fe56f0f3d55
Miron <miron.nicolaevich@gmail.com>
2024-10-01 22:38:18 +0200

    feat(database): add common func: create_token

--- src/core/controllers/depends/utils/jwt_token.py
+++ src/core/controllers/depends/utils/jwt_token.py
@@ -4,6 +4,7 @@ import datetime
 
 import jwt
 
+from src.core.settings.const import JWT
 from src.core.settings.settings import settings
 
 
@@ -23,8 +24,8 @@ def encode_jwt(
         expire = now + datetime.timedelta(minutes=expire_minutes)
 
     to_encode = payload.copy()
-    to_encode["exp"] = expire
-    to_encode["iat"] = now
+    to_encode[JWT.PAYLOAD_EXPIRE_KEY] = expire
+    to_encode[JWT.PAYLOAD_IAT_KEY] = now
     encode = jwt.encode(
         payload=to_encode,
         key=private_key,
@@ -45,3 +46,36 @@ def decode_jwt(
         algorithms=[algorithm],
     )
     return decoded
+
+
+def create_token(
+    payload: dict,
+    type_token: str,
+    expire_minutes: int = settings.jwt_tokens.access_token_expire_minutes,
+    expire_delta: datetime.timedelta | None = None,
+) -> str:
+    """Create new jwt token ot refresh token.
+
+    Args:
+        type_token (str): access or refresh.
+        payload (dict): payload token.
+        expire_minutes (int): for access token.
+        expire_delta (int): for refresh token.
+    Returns:
+        jwt token (str)
+    """
+    jwt_payload = {JWT.TOKEN_TYPE_FIELD: type_token}
+    jwt_payload.update(payload)
+    set_expire_delta: datetime.timedelta | None = (
+        expire_delta
+        if type_token == JWT.TOKEN_TYPE_ACCESS
+        else datetime.timedelta(
+            days=settings.jwt_tokens.refresh_token_expire_days
+        )
+    )
+
+    return encode_jwt(
+        payload=jwt_payload,
+        expire_minutes=expire_minutes,
+        expire_delta=set_expire_delta,
+    )
