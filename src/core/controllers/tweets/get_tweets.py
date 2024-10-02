f5506dd1dc395be4c3a3491060dc59f0b9086db6
Miron-Anosov <mairon26rus@gmail.com>
2024-10-02 23:32:15 +0200

    feat(database): complete get_tweets

--- src/core/controllers/tweets/get_tweets.py
+++ src/core/controllers/tweets/get_tweets.py
@@ -1,13 +1,20 @@
 """GET /tweets/."""
 
+from typing import Annotated
+
+from fastapi import Depends
+
+from src.core.controllers.depends.tweets.get_tweets import get_tweets_data
 from src.core.validators import GetAllTweets
 
 
-async def get_tweets() -> "GetAllTweets":
+async def get_tweets(
+    tweets: Annotated["GetAllTweets", Depends(get_tweets_data)]
+) -> "GetAllTweets":
     """
     Get tweets.
 
      **Headers**:
-    - `x-auth-token (str)*`: User key authentication.
+    - Authorization: Bearer `access_token` (str): User key authentication.
     """
-    return GetAllTweets()
+    return tweets
