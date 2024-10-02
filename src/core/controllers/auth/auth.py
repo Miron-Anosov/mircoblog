1a4ff19f14b0034565ee04c45e5c21a1c8be249c
Miron-Anosov <mairon26rus@gmail.com>
2024-10-02 02:40:14 +0200

    feat(database): create logout and push tokens

--- src/core/controllers/auth/auth.py
+++ src/core/controllers/auth/auth.py
@@ -4,23 +4,25 @@ Routes:
     - POST /api/auth/login
     - POST /api/auth/logout
     - POST /api/auth/users
+    - DELETE /api/auth/logout
 """
 
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
+from fastapi.security import APIKeyCookie, HTTPBearer
 
+from src.core.controllers.depends.check_token import update_tokens_by_refresh
 from src.core.controllers.depends.login_user import (
     login_user_form,
     login_user_json,
 )
 from src.core.controllers.depends.new_user_form import create_new_user_form
 from src.core.controllers.depends.new_user_json import create_new_user_json
+from src.core.settings.const import JWT, Headers
 from src.core.settings.routes_path import AuthRoutes
 from src.core.validators import StatusResponse, UserToken
-
-# from fastapi.responses import RedirectResponse
 
 
 def create_auth_route() -> APIRouter:
@@ -29,7 +31,10 @@ def create_auth_route() -> APIRouter:
     Return:
     - APIRouter
     """
-    return APIRouter(tags=[AuthRoutes.TAG], prefix=AuthRoutes.PREFIX)
+    return APIRouter(
+        tags=[AuthRoutes.TAG],
+        prefix=AuthRoutes.PREFIX,
+    )
 
 
 auth: APIRouter = create_auth_route()
@@ -85,11 +90,11 @@ async def new_user_json(
 
 @auth.post(
     path=AuthRoutes.POST_LOGIN_USER_JSON,
-    status_code=status.HTTP_200_OK,
+    status_code=status.HTTP_201_CREATED,
     response_model=UserToken,
 )
 async def login_json(
-    user_token: Annotated[UserToken, Depends(login_user_json)]
+    user_token: Annotated["JSONResponse", Depends(login_user_json)],
 ) -> "JSONResponse":
     """**Post loging user**.
 
@@ -97,24 +102,18 @@ async def login_json(
     - `name` (str): user's name.
     - `password` (str): user's secret.
 
+    Set Cookie:  `refresh_token`
     """
-    # response.set_cookie(key="x-auth-token", value="test-token")
-
-    return JSONResponse(
-        content=user_token.model_dump(),
-        status_code=status.HTTP_200_OK,
-        media_type="application/json",
-        headers={"x-auth-token": "TEST"},
-    )
+    return user_token
 
 
 @auth.post(
     path=AuthRoutes.POST_LOGIN_USER_FORM,
-    status_code=status.HTTP_200_OK,
+    status_code=status.HTTP_201_CREATED,
     response_model=UserToken,
 )
 async def login_form(
-    user_token: Annotated[UserToken, Depends(login_user_form)]
+    users_tokens: Annotated["JSONResponse", Depends(login_user_form)],
 ) -> "JSONResponse":
     """**Post loging user**.
 
@@ -122,12 +121,49 @@ async def login_form(
     - `name` (str): user's name.
     - `password` (str): user's secret.
 
+    Set Cookie:  `refresh_token`
     """
-    # response.set_cookie(key="x-auth-token", value="test-token")
+    return users_tokens
 
-    return JSONResponse(
-        content=user_token.model_dump(),
+
+@auth.patch(
+    path=AuthRoutes.PATCH_TOKENS,
+    status_code=status.HTTP_201_CREATED,
+    response_model=UserToken,
+)
+async def patch_access_token(
+    refresh_token: Annotated["JSONResponse", Depends(update_tokens_by_refresh)]
+) -> "JSONResponse":
+    """**Path access and refresh token**.
+
+    Requirement :
+        - Cookie:  `refresh_token`
+    """
+    return refresh_token
+
+
+@auth.delete(
+    path=AuthRoutes.DEL_LOGOUT_USER,
+    status_code=status.HTTP_200_OK,
+    response_model=StatusResponse,
+    dependencies=[
+        Depends(HTTPBearer()),
+        Depends(APIKeyCookie(name=JWT.TOKEN_TYPE_REFRESH)),
+    ],
+)
+async def logout_user() -> "JSONResponse":
+    """**Delete loging user**.
+
+    Requirement :
+        - Cookie:  `refresh_token`
+        - Authorization: Bearer
+
+    """
+    response = JSONResponse(
+        content=StatusResponse().model_dump(),
         status_code=status.HTTP_200_OK,
-        media_type="application/json",
-        headers={"x-auth-token": "TEST"},
     )
+    response.delete_cookie(key=JWT.TOKEN_TYPE_REFRESH)
+    response.headers.update(Headers.WWW_AUTH_BEARER_LOGOUT)
+    response.headers.update(Headers.AUTHORIZATION)
+    return response
