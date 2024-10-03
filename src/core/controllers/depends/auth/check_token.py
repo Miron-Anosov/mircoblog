295cd37ebcec1ab2d476b2ec275db4164071a44b
Miron-Anosov <mairon26rus@gmail.com>
2024-10-03 17:36:00 +0200

    Reapply "feat(database): add try/except for db"
    This reverts commit a554234d9e29bb269fa07c2c8e052c5364a242ac.

--- src/core/controllers/depends/auth/check_token.py
+++ src/core/controllers/depends/auth/check_token.py
@@ -2,38 +2,21 @@
 
 from typing import Annotated
 
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends
 from fastapi.security import APIKeyCookie, OAuth2PasswordBearer
 from jwt.exceptions import InvalidTokenError
 
 from src.core.controllers.depends.utils.jsonresponse_new_jwt import response
 from src.core.controllers.depends.utils.jwt_token import decode_jwt
-from src.core.settings.const import JWT, Headers, MessageError, TypeEncoding
+from src.core.controllers.depends.utils.return_error import http_exception
+from src.core.settings.const import JWT, Headers, TypeEncoding
 from src.core.settings.routes_path import AuthRoutes
-from src.core.validators import ErrResp
 
 oauth_bearer = OAuth2PasswordBearer(
     tokenUrl=f"{AuthRoutes.PREFIX}{AuthRoutes.POST_LOGIN_USER_FORM}",
 )
 
 cookie_refresh = APIKeyCookie(name=JWT.TOKEN_TYPE_REFRESH)
-
-
-def http_exception(
-    status_code: int = status.HTTP_401_UNAUTHORIZED,
-    error_type=MessageError.INVALID_TOKEN_ERR,
-    error_message=MessageError.INVALID_TOKEN_ERR_MESSAGE,
-    headers=Headers.WWW_AUTH_BEARER,
-) -> HTTPException:
-    """Return HTTPException."""
-    return HTTPException(
-        status_code=status_code,
-        detail=ErrResp(
-            error_type=error_type,
-            error_message=error_message,
-        ).model_dump(),
-        headers=headers,
-    )
 
 
 async def token_is_alive(
@@ -51,7 +34,7 @@ async def token_is_alive(
     try:
         return decode_jwt(jwt_token=token)
     except InvalidTokenError:
-        raise http_exception()
+        raise http_exception(headers=Headers.WWW_AUTH_BEARER)
 
 
 async def refresh_token_is_alive(
@@ -71,7 +54,7 @@ async def refresh_token_is_alive(
             jwt_token=old_refresh_token.encode(TypeEncoding.UTF8)
         )
     except InvalidTokenError:
-        raise http_exception()
+        raise http_exception(headers=Headers.WWW_AUTH_BEARER)
 
 
 async def get_user_id_by_token_access(
@@ -96,7 +79,7 @@ async def get_user_id_by_token_access(
         raise InvalidTokenError
 
     except InvalidTokenError:
-        raise http_exception()
+        raise http_exception(headers=Headers.WWW_AUTH_BEARER)
 
 
 async def up_tokens_by_refresh(
--- src/core/models_orm/crud_models/tweet_crud.py
+++ src/core/models_orm/crud_models/tweet_crud.py
@@ -1,12 +1,15 @@
 """Tweets CRUD methods."""
 
 import abc
-from typing import TYPE_CHECKING
+import uuid
+from typing import Any, Sequence
 
+from sqlalchemy import Row, RowMapping, select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 
-if TYPE_CHECKING:
-    from src.core.models_orm.models.tweet_orm import TweetsORM
+from src.core.models_orm.crud_models.utils.catcher_errors import cather_sql_err
+from src.core.models_orm.models.tweet_orm import TweetsORM
 
 
 class _TweetInterface(abc.ABC):
@@ -31,8 +34,12 @@ class _TweetInterface(abc.ABC):
     @staticmethod
     @abc.abstractmethod
     async def post_tweet(
-        session: AsyncSession, content: str, model: "TweetsORM"
-    ) -> int:
+        session: AsyncSession,
+        user_id: int,
+        content_test: str,
+        content_media_ids: list[int],
+        table=TweetsORM,
+    ) -> str:
         """Create new tweet."""
         pass
 
@@ -45,11 +52,18 @@ class _TweetInterface(abc.ABC):
         """Delete tweet by id."""
         pass
 
+    @staticmethod
+    @abc.abstractmethod
+    async def get_tweets(session: AsyncSession, model: "TweetsORM") -> None:
+        """Create like for tweet by id tweet."""
+        pass
+
 
 class Tweets(_TweetInterface):
     """Tweets CRUD methods."""
 
     @staticmethod
+    @cather_sql_err
     async def post_like(session, id_tweet, model) -> None:
         """Create like for tweet by id tweet."""
         _ = await session.get(id_tweet, model)
@@ -62,15 +76,26 @@ class Tweets(_TweetInterface):
         return True  # TODO: miss to make logic
 
     @staticmethod
+    @cather_sql_err
     async def post_tweet(
-        session,
-        content: str,
-        model,
-    ) -> int:
+        session: AsyncSession,
+        user_id: str,
+        content_test: str,
+        content_media_ids: list[int],
+        table=TweetsORM,
+    ) -> str | None:
         """Create new tweet."""
-        return 1  # TODO: miss to make logic
+        new_tweet = table(
+            id=uuid.uuid4().hex, content=content_test, author=user_id
+        )
+        con = await session.begin()
+        session.add(new_tweet)
+        await con.commit()
+
+        return new_tweet.id
 
     @staticmethod
+    @cather_sql_err
     async def delete_tweet(
         session: AsyncSession,
         id_tweet: int,
@@ -78,3 +103,17 @@ class Tweets(_TweetInterface):
         """Delete tweet by id."""
         print("delete tweet")
         return True  # TODO: miss to make logic
+
+    @staticmethod
+    @cather_sql_err
+    async def get_tweets(
+        session: AsyncSession, model_tweets: TweetsORM = TweetsORM
+    ) -> Sequence[Row[Any] | RowMapping | Any] | None:
+        """Create like for tweet by id tweet."""
+        query = select(model_tweets).options(
+            selectinload(model_tweets.likes),
+            selectinload(model_tweets.attachments),
+            selectinload(model_tweets.owner),
+        )
+        tweets = await session.scalars(query)
+        return tweets.all()
