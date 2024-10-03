e42beb97180bcf7f6c4c28177b2b5903d6bc7c4e
Miron <miron.nicolaevich@gmail.com>
2024-10-04 00:47:12 +0200

    docs(database): add some info

--- src/core/controllers/depends/utils/return_error.py
+++ src/core/controllers/depends/utils/return_error.py
@@ -2,7 +2,7 @@
 
 from fastapi import HTTPException, status
 
-from src.core.settings.const import Headers, MessageError
+from src.core.settings.const import MessageError
 from src.core.validators import ErrResp
 
 
@@ -12,7 +12,17 @@ def http_exception(
     error_message=MessageError.INVALID_TOKEN_ERR_MESSAGE,
     headers: dict[str, str] | None = None,
 ) -> HTTPException:
-    """Return HTTPException."""
+    """Return HTTPException.
+
+    Args:
+        - status_code (int): HTTP status.
+        - error_type (str): Type error.
+        - error_message (str): Message error.
+        - headers (dict[str, str] | None): If it is requirement.
+    Raise:
+        - HTTPException
+
+    """
     return HTTPException(
         status_code=status_code,
         detail=ErrResp(
