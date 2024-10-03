4212a950243d16f78b080ec22afcc1d78bbe41b4
Miron <miron.nicolaevich@gmail.com>
2024-10-03 14:02:57 +0200

    feat(database): create get_tweet_data

--- src/core/controllers/depends/tweets/get_tweets.py
+++ src/core/controllers/depends/tweets/get_tweets.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING, Annotated
 from fastapi import Depends
 
 from src.core.controllers.depends.connect_db import get_crud, get_session
-from src.core.validators import GetAllTweets
+from src.core.validators import GetAllTweets, Like, User
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
@@ -19,4 +19,24 @@ async def get_tweets_data(
 ) -> "GetAllTweets":
     """Return tweets."""
     tweets = await crud.tweets.get_tweets(session=session)
-    return GetAllTweets(tweets=tweets)
+    return GetAllTweets(
+        tweets=[
+            dict(
+                id=str(tweet.id),
+                content=tweet.content,
+                attachments=tweet.attachments,
+                author=User(
+                    id=str(tweet.owner.id),
+                    name=tweet.owner.name,
+                ),
+                likes=[
+                    Like(
+                        user_id=user_like.id,
+                        name=user_like.name,
+                    )
+                    for user_like in tweet.likes
+                ],
+            )
+            for tweet in tweets
+        ]
+    )
