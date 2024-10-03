155118753c427e8d5ed1b84305ebad101e74ef12
Miron-Anosov <mairon26rus@gmail.com>
2024-10-03 21:29:49 +0200

    refactor(database): change signature for del_tweet_by_id

--- src/core/controllers/tweets/del_tweet.py
+++ src/core/controllers/tweets/del_tweet.py
@@ -3,17 +3,24 @@
 Delete tweet.
 """
 
+from typing import Annotated
+
+from fastapi import Depends
+
+from src.core.controllers.depends.tweets.del_tweet import del_tweet
 from src.core.validators import StatusResponse
 
 
-async def del_tweet_by_id(tweet_id: str) -> "StatusResponse":
+async def del_tweet_by_id(
+    result: Annotated[bool, Depends(del_tweet)]
+) -> "StatusResponse":
     """
     Delete a tweet by ID.
 
      **Headers**:
-    - `x-auth-token (str)*`: User key authentication.
+    - Authorization: Bearer `access_token` (str): User key authentication.
 
     **Path Parameters**:
-    - `tweet_id (int)`: The ID of the tweet to delete.
+    - `tweet_id (str)`: The ID of the tweet to delete.
     """
-    return StatusResponse()
+    return StatusResponse(result=result)
