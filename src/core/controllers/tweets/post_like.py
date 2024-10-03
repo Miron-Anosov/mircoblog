268fd5f0ae386bd0a20c5d63ba1cc7f8f06ede19
Miron <miron.nicolaevich@gmail.com>
2024-10-04 00:14:37 +0200

    refactor(databse): change signatura for post_like_by_id

--- src/core/controllers/tweets/post_like.py
+++ src/core/controllers/tweets/post_like.py
@@ -3,17 +3,24 @@
 Create like of tweet.
 """
 
+from typing import Annotated
+
+from fastapi import Depends
+
+from src.core.controllers.depends.tweets.like import post_like
 from src.core.validators import StatusResponse
 
 
-async def post_like_by_id(tweet_id: str) -> "StatusResponse":
+async def post_like_by_id(
+    result: Annotated[bool, Depends(post_like)]
+) -> "StatusResponse":
     """
     Like a tweet by ID.
 
      **Headers**:
-    - `x-auth-token (str)*`: User key authentication.
+    - Authorization: Bearer `access_token` (str): User key authentication.
 
     **Path Parameters**:
-    - `tweet_id (int)`: The ID of the tweet to like.
+    - `tweet_id (str)`: The ID of the tweet to like.
     """
     return StatusResponse()
