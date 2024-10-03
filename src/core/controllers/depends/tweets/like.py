e83130f72e5efd59cc3321de5e5dd7e3ec64896c
Miron-Anosov <mairon26rus@gmail.com>
2024-10-04 01:17:42 +0200

    feat(database): create delete_loke /api/tweets/{id}/like

--- src/core/controllers/depends/tweets/like.py
+++ src/core/controllers/depends/tweets/like.py
@@ -3,7 +3,7 @@
 /api/tweets/{id}/like
 """
 
-from typing import TYPE_CHECKING, Annotated
+from typing import TYPE_CHECKING, Annotated, Optional
 
 from fastapi import Depends, status
 
@@ -30,5 +30,42 @@ async def post_like(
     like_result = await crud.tweets.post_like(
         session=session, id_user=id_user, id_tweet=tweet_id
     )
-    print(like_result)
+
+    raise_http(is_none_result=like_result)
+
     return True
+
+
+async def delete_like(
+    tweet_id: str,
+    id_user: Annotated[str, Depends(get_user_id_by_token_access)],
+    session: Annotated["AsyncSession", Depends(get_session)],
+    crud: Annotated["Crud", Depends(get_crud)],
+) -> bool:
+    """Create like for some tweets."""
+    like_result = await crud.tweets.delete_like(
+        session=session, id_user=id_user, id_tweet=tweet_id
+    )
+
+    raise_http(is_none_result=like_result)
+
+    return True
+
+
+def raise_http(is_none_result: Optional[bool]) -> None:
+    """Checker None.
+
+    Args:
+        - is_none_result (Optional[bool]): result db.
+    Raise:
+        - HTTPException
+    Nones:
+        - If db finish incorrect a process than it'll return None.
+    """
+    if is_none_result is None:
+        # todo: add logger info fail get tweets data from db
+        raise http_exception(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            error_type=MessageError.TYPE_ERROR_INTERNAL_SERVER_ERROR,
+            error_message=MessageError.MESSAGE_SERVER_ERROR,
+        )
--- src/core/controllers/tweets/del_like.py
+++ src/core/controllers/tweets/del_like.py
@@ -3,17 +3,22 @@
 del like of tweet.
 """
 
+from typing import Annotated
+
+from fastapi import Depends
+
+from src.core.controllers.depends.tweets.like import delete_like
 from src.core.validators import StatusResponse
 
 
-async def del_like(tweet_id: str) -> StatusResponse:
+async def del_like(_: Annotated[bool, Depends(delete_like)]) -> StatusResponse:
     """
     Del Like a tweet by ID.
 
      **Headers**:
-    - `x-auth-token (str)*`: User key authentication.
+    - Authorization: Bearer `access_token` (str): User key authentication.
 
     **Path Parameters**:
-    - `tweet_id (int)`: The ID of the tweet to delete.
+    - `tweet_id (str)`: The ID of the tweet to delete a like.
     """
     return StatusResponse()
