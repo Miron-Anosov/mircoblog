f963203a2ea6ea0e22cd3a11de11bd0bf6b7fa24
Miron <miron.nicolaevich@gmail.com>
2024-10-05 19:07:25 +0200

    docs(database): update docs

--- src/core/models_orm/models/media_orm.py
+++ src/core/models_orm/models/media_orm.py
@@ -2,7 +2,7 @@
 
 from typing import TYPE_CHECKING
 
-from sqlalchemy import BIGINT, ForeignKey
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.models_orm.models.base_model import BaseModel
@@ -15,13 +15,20 @@ class MediaORM(BaseModel):
     """Media ORM model.
 
     Table: media
+
+        CREATE TABLE media (
+        id SERIAL NOT NULL,
+        links VARCHAR NOT NULL,
+        tweet_id UUID NOT NULL,
+        PRIMARY KEY (id),
+        UNIQUE (links),
+        FOREIGN KEY(tweet_id) REFERENCES tweets (id)
+    )
     """
 
     __tablename__ = "media"
     __mapper_args__ = {"eager_defaults": True}
-    id: Mapped[int] = mapped_column(
-        BIGINT, primary_key=True, autoincrement=True
-    )
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     links: Mapped[str] = mapped_column(unique=True)
     tweet_id: Mapped[str] = mapped_column(ForeignKey("tweets.id"))
     tweets: Mapped["TweetsORM"] = relationship(back_populates="attachments")
