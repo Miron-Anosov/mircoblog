a646d74fd40246c2673c4ded9edd3309789b7afa
Miron-Anosov <mairon26rus@gmail.com>
2024-10-05 19:07:25 +0200

    docs(database): update docs

--- src/core/models_orm/models/likes_models.py
+++ src/core/models_orm/models/likes_models.py
@@ -2,7 +2,7 @@
 
 from typing import TYPE_CHECKING
 
-from sqlalchemy import BIGINT, UUID, ForeignKey
+from sqlalchemy import UUID, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.core.models_orm.models.base_model import BaseModel
@@ -16,13 +16,20 @@ class LikesORM(BaseModel):
     """Likes ORM model.
 
     Table: likes
+
+        CREATE TABLE likes (
+        like_id SERIAL NOT NULL,
+        tweet_id UUID NOT NULL,
+        user_id UUID NOT NULL,
+        PRIMARY KEY (like_id),
+        FOREIGN KEY(tweet_id) REFERENCES tweets (id),
+        FOREIGN KEY(user_id) REFERENCES users (id)
+    )
     """
 
     __tablename__ = "likes"
     __mapper_args__ = {"eager_defaults": True}
-    like_id: Mapped[int] = mapped_column(
-        BIGINT, primary_key=True, autoincrement=True
-    )
+    like_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     tweet_id: Mapped[UUID] = mapped_column(ForeignKey("tweets.id"))
     user_id: Mapped[UUID] = mapped_column(ForeignKey("users.id"))
     tweet: Mapped["TweetsORM"] = relationship(
