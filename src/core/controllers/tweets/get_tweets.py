bb8d8987a5e38a0ed54604b15fa4b1d2e79abd36
Miron-Anosov <mairon26rus@gmail.com>
2024-10-11 01:00:04 +0200

    feat(confServser): add Redis server

--- src/core/controllers/tweets/get_tweets.py
+++ src/core/controllers/tweets/get_tweets.py
@@ -3,11 +3,14 @@
 from typing import Annotated
 
 from fastapi import Depends
+from fastapi_cache.decorator import cache
 
 from src.core.controllers.depends.tweets.get_tweets import get_tweets_data
+from src.core.settings.const import CacheExpirationTime
 from src.core.validators import GetAllTweets
 
 
+@cache(expire=CacheExpirationTime.CACHE_EXPIRATION_TIME_GET_TWEETS)
 async def get_tweets(
     tweets: Annotated["GetAllTweets", Depends(get_tweets_data)]
 ) -> "GetAllTweets":
--- src/main.py
+++ src/main.py
@@ -15,18 +15,37 @@ Functional ability:
 
 """
 
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
+from redis import asyncio as aioredis
 
 from src.core.controllers.auth.auth import auth
 from src.core.controllers.media.media import media
 from src.core.controllers.tweets.main_tweets import tweets
 from src.core.controllers.users.users import users
 from src.core.settings import swagger_info
+from src.core.settings.const import TypeEncoding
 from src.core.settings.routes_path import AuthRoutes
+from src.core.settings.settings import settings
 
 
-def create_app() -> FastAPI:
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    """Init cache."""
+    redis = await aioredis.from_url(
+        settings.redis.REDIS_URL,
+        encoding=TypeEncoding.UTF8,
+        decode_responses=True,
+    )
+    FastAPICache.init(RedisBackend(redis), prefix=settings.redis.PREFIX)
+    yield
+
+
+async def create_app() -> FastAPI:
     """Maker FastAPI."""
     app_ = FastAPI(
         title=swagger_info.TITLE,
@@ -37,21 +56,24 @@ def create_app() -> FastAPI:
         servers=swagger_info.SERVERS,
         summary=swagger_info.SUMMARY,
         root_path=AuthRoutes.PREFIX,
+        lifespan=lifespan,
     )
     # todo: logger conf app DEBUG
     app_.include_router(router=media)
     app_.include_router(router=tweets)
     app_.include_router(router=users)
     app_.include_router(router=auth)
+
     return app_
 
 
 if __name__ == "__main__":
-    app: FastAPI = create_app()
+    import asyncio
+
+    app = asyncio.run(create_app())
     uvicorn.run(
-        create_app,
+        app,
         host="0.0.0.0",
         port=8001,
         log_level="info",
-        factory=True,
     )
