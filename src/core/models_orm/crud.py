c11eaa0335f91a9d5284d189c98296c0df2e6f8a
Miron-Anosov <mairon26rus@gmail.com>
2024-10-18 19:07:06 +0200

    docs(confServer): up docs

--- src/core/models_orm/crud.py
+++ src/core/models_orm/crud.py
@@ -1,4 +1,17 @@
-"""Core ORM."""
+"""
+Core ORM module.
+
+This module provides a combined interface for handling CRUD operations
+across multiple models. It simplifies access to CRUD functionality by
+grouping operations for tweets, users, media, and authentication under
+a single class, `Crud`.
+
+Classes:
+    Crud: Combines CRUD operations for tweets, users, media, and auth.
+
+Functions:
+    create_crud_helper: Creates and returns a `Crud` instance.
+"""
 
 from src.core.models_orm.crud_models.auth_crud import AuthUsers
 from src.core.models_orm.crud_models.media_crud import Media
@@ -7,7 +20,14 @@ from src.core.models_orm.crud_models.user_crud import Users
 
 
 class Crud:
-    """Combined interface for all CRUD operations."""
+    """Combined interface for all CRUD operations.
+
+    Attributes:
+        tweets: CRUD operations for tweet model.
+        users: CRUD operations for user model.
+        media: CRUD operations for media model.
+        auth_users: CRUD operations for auth model.
+    """
 
     def __init__(
         self,
@@ -17,13 +37,13 @@ class Crud:
         auth_crud: "AuthUsers",
     ) -> None:
         """
-        DB worker and crud models.
+        Initialize Crud with specific CRUD instances.
 
-        db_worker: ManagerDB
-        tweets_crud: Tweets()
-        user_crud: Users()
-        media_crud: Media()
-        auth_crud: AuthUsers()
+        Args:
+            tweets_crud: Instance of Tweets CRUD.
+            user_crud: Instance of Users CRUD.
+            media_crud: Instance of Media CRUD.
+            auth_crud: Instance of AuthUsers CRUD.
         """
         self.tweets = tweets_crud
         self.users = user_crud
@@ -31,8 +51,12 @@ class Crud:
         self.auth_users = auth_crud
 
 
-def create_crud_halper() -> Crud:
-    """Return CRUD object for all models."""
+def create_crud_helper() -> Crud:
+    """Create and return a Crud object for all models.
+
+    Returns:
+        Crud: An instance of the Crud class with initialized models.
+    """
     create_db = Crud(
         tweets_crud=Tweets(),
         user_crud=Users(),
